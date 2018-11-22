68caf485be6fb1136f6f66b95d5c3f81ee130c9b
Alian <alian@aleph.engineering>
2018-11-22 11:21:04 -0500

    Receives start and end date in env vars

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import gitlab
 import datetime
+import dateutil.parser
 import requests
 import os
 
@@ -14,23 +15,28 @@ private_token = os.getenv('GQ_PRIVATE_TOKEN')
 url = os.getenv('GQ_URL')
 assignee = os.getenv('GQ_ASSIGNEE')
 label = os.getenv('GQ_LABEL')
+start_date = dateutil.parser.parse(os.getenv('GQ_START_DATE'))
+end_date = dateutil.parser.parse(os.getenv('GQ_END_DATE'))
 
-date_start = datetime.datetime(2018, 10, 1)
-date_end = datetime.datetime(2018, 12, 1)
+# TODO: get by env vars
+# date_start = datetime.datetime(2018, 10, 1)
+# date_end = datetime.datetime(2018, 12, 1)
 
 gl = gitlab.Gitlab(url, private_token, api_version=4, session=session)
 issues = gl.issues.list()
 
 issues_filtered = list(filter(lambda x: x.assignee is not None and
                                         x.updated_at is not None and
+                                        x.labels is not None and
                                         x.assignee['username'] == assignee and
                                         x.labels.__contains__(label) and
-                            date_start < datetime.datetime.strptime(x.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ") < date_end,
+                            start_date< datetime.datetime.strptime(x.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ") < end_date,
                        issues))
 
 time_spent = sum(issue.time_stats['total_time_spent'] for issue in issues_filtered)
+# total_weight = sum(issue.weight for issue in issues_filtered)
 
 print('User: ' + assignee)
-print('Period: From {} to {}'.format(date_start, date_end))
+print('Period: From {} to {}'.format(start_date, end_date))
 print('Tasks completed:' + str(len(issues_filtered)))
 print('Time spent in hours: ' + str(datetime.timedelta(seconds=time_spent)))
