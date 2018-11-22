303572801d71c196dad0a657e042924d1418145d
Alian <alian@aleph.engineering>
2018-11-22 06:39:42 -0500

    Rename variable

--- script.py
+++ script.py
@@ -21,14 +21,14 @@ date_end = datetime.datetime(2018, 12, 1)
 gl = gitlab.Gitlab(url, private_token, api_version=4, session=session)
 issues = gl.issues.list()
 
-tasks = list(filter(lambda x: x.assignee['username'] == assignee and
+issues_filtered = list(filter(lambda x: x.assignee['username'] == assignee and
                            x.labels.__contains__(label) and
                            date_start < datetime.datetime.strptime(x.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ") < date_end,
                        issues))
 
-time_spent = sum(task.time_stats['total_time_spent'] for task in tasks)
+time_spent = sum(issue.time_stats['total_time_spent'] for issue in issues_filtered)
 
 print('User: ' + assignee)
 print('Period: From {} to {}'.format(date_start, date_end))
-print('Tasks completed:' + str(len(tasks)))
+print('Tasks completed:' + str(len(issues_filtered)))
 print('Time spent in hours: ' + str(datetime.timedelta(seconds=time_spent)))
