98b93f7db25a4dfb61038fb14b9127c8b9642044
Alian <alian@aleph.engineering>
2018-11-21 22:23:46 -0500

    Format period of query

--- script.py
+++ script.py
@@ -21,11 +21,10 @@ date_end = datetime.datetime(2018, 12, 1)
 gl = gitlab.Gitlab(url, private_token, api_version=4, session=session)
 issues = gl.issues.list()
 
-issues_filter = filter(lambda x: x.assignee['username'] == assignee and
+tasks = list(filter(lambda x: x.assignee['username'] == assignee and
                                  x.labels.__contains__(label) and
                                  date_start < datetime.datetime.strptime(x.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ") < date_end,
-                       issues)
-tasks = list(issues_filter)
+                       issues))
 
 sum_estimated = 0
 for issue in tasks:
@@ -34,4 +33,4 @@ for issue in tasks:
 print('User: ' + assignee)
 print('Period: From {} to {}'.format(date_start, date_end) )
 print('Tasks completed:' + str(len(tasks)))
-print('Time spent: ' + str(datetime.timedelta(seconds=sum_estimated)))
+print('Time spent in hours: ' + str(datetime.timedelta(seconds=sum_estimated)))
