d961b8c102b74d50fc96df02ee68e042237d2241
Alian <alian@aleph.engineering>
2018-11-23 08:06:55 -0500

    Refactor ordering

--- script.py
+++ script.py
@@ -22,22 +22,24 @@ end_date = dateutil.parser.parse(os.getenv('GQ_END_DATE'))
 gl = gitlab.Gitlab(url, private_token, api_version=4, session=session)
 issues = gl.issues.list()
 
-issues_filtered = list(filter(lambda x: x.assignee is not None and
+def sort_issues(i):
+    return i.project_id
+
+
+issues_sorted = \
+    sorted(
+        list(
+            filter(lambda x: x.assignee is not None and
                                         x.updated_at is not None and
                                         x.labels is not None and
                                         x.assignee['username'] == assignee and
                                         x.labels.__contains__(label) and
                             start_date< datetime.datetime.strptime(x.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ") < end_date,
-                       issues))
+                       issues)), key=sort_issues)
 
-def sort_issues(i):
-    return i.project_id
+groups_by_project = itertools.groupby(issues_sorted, sort_issues)
 
-
-issues_sorted = sorted(issues_filtered, key=sort_issues)
-group_by_project = itertools.groupby(issues_sorted, sort_issues)
-
-for id, issuesGroup in group_by_project:
+for id, issuesGroup in groups_by_project:
     project_issues = list(issuesGroup)
     time_spent = sum(issue.time_stats['total_time_spent'] for issue in project_issues)
     time_estimate = sum(issue.time_stats['time_estimate'] for issue in project_issues)
